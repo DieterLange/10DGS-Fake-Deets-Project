6ca8f5dbe62cc4a5399b38d0d3cc6715eae36bca
DieterLange <51689600+DieterLange@users.noreply.github.com>
2019-07-30 15:13:12 +1000

    Start dat (30/07/19)

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+</head>
+<body style="background-color: navy;">
+    <div style="font-family: Arial, Helvetica, sans-serif; display: flex; justify-content: center; flex-direction: column; text-align: center; border: clear; background-color: white">
+        <div style="margin-top: 2%;">
+            <h1>Welcome to Fake Deetz!</h1>
+        </div>
+            <div>
+                <button>
+            </div>
+    </div>
+</body>
+</html>
