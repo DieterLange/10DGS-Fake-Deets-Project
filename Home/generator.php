6226eea0648163dfbb93405a5338ec3b4a9d32dd
DieterLange <51689600+DieterLange@users.noreply.github.com>
2019-08-20 10:13:15 +1000

    Generator

--- Home/generator.php
+++ Home/generator.php
@@ -14,27 +14,29 @@
             <a href="http://localhost/10DGS-Fake-Deets-Project/Admin/admin.php" style="text-decoration: none; margin-right: 70px;"><font color="#fafaf">Admin</a>
             <a href="http://localhost/10DGS-Fake-Deets-Project/Home/contact.php" style="text-decoration: none; margin-right: 70px;"><font color="#fafaf">Contact</a>
         </div>
+        <font color="#000">
         <?php
-             $servername = "localhost";
-             $username = "root";
-             $password = "root";
-             $dbname = "fake deets";
-             $conn = new mysqli($servername, $username, $password, $dbname);
-             if ($conn->connect_error)   {
-             die("Connection failed: " . $conn->connect_error);
-             }
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $dbname = "fake deets";
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error)   {
+            die("Connection failed: " . $conn->connect_error);
+            }
 
-             $sql = "SELECT * FROM userinfo ORDER BY RAND() LIMIT 1";
-             $result = mysqli_query($conn, $sql);
-        ?>
-        <?php
-             while($user=mysqli_fetch_array($result)) {
+            $sql = "SELECT * FROM userinfo ORDER BY RAND() LIMIT 1";
+            $results = mysqli_query($conn,$sql);
+
+            if (mysqli_num_rows($results)>0) {
+
+                while($row=mysqli_fetch_array($results)) {
+                    echo "Name: ".$row[1]." ".$row[2]."<br><br< Gender: ".$row[3]."<br><br> Date of Birth: ".$row[4]."<br><br> Email Adress: ".$row[5]."<br><br> Home Adress: ".$row[6]."<br><br> Phone Number: ".$row[7]."<br><br> Photo: ".$row[8];
+                }
+            }
             
-                echo "$user['fname']";
-                echo "$user['lname']";
-                echo "$user['gender']";
-             }
         ?>
+        </font>
     </div>
 </body>
 </html>
